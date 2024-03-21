e5aed16079d7dca37dc19e5bc1723db3fe813b19
Joranne <dolcie.joranne@gmail.com>
2024-03-22 00:58:32 +0100

    FIX/ Header + ADD/ Main and Footer

---
+++ src/components/Main/Gallery/Thumbnail.js
@@ -0,0 +1,13 @@
+import thumbnail_img from '../../../assets/thumbnail.png'
+//import '../styles/Header.css'
+
+function Thumbnail() {
+    const thumbnail_title = 'Titre de la location'
+
+    return  <div className='thumbnail'>
+                <img src={thumbnail_img} alt='kasa-thumbnail'></img>
+                <h2>{thumbnail_title}</h2>
+            </div> 
+}
+
+export default Thumbnail
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import Banner from './Banner/Banner.js'
+import Gallery from './Gallery/Gallery.js'
+
+function Main () {
+    return (
+        <>
+            <Banner/>
+            <Gallery/>
+        </>
+    );
+}
+
+export default Main
