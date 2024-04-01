aa50ca983abca83fe23bc90472d9ad6bd3396a06
Joranne <dolcie.joranne@gmail.com>
2024-04-01 22:56:24 +0200

    ADD// Header.scss + Banner.scss FIX// index.css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import logo from '../../assets/logo_kasa.png'
-//import '../styles/Header.css'
+import '../Header/Header.scss'
 
 function Header() {
     const link_name_1 = 'Acceuil'
@@ -8,8 +8,8 @@ function Header() {
     const link_2 = 'https://www.google.com/'
 
     return  <nav className='kasa-header'>
-                <img src={logo} alt='kasa-logo'></img>
-                <ul>
+                <img src={logo}  className='kasa-logo' alt='kasa-logo'></img>
+                <ul className='kasa-header-links'>
                     <li><a href={link_1}>{link_name_1}</a></li>
                     <li><a href={link_2}>{link_name_2}</a></li>
                 </ul>
