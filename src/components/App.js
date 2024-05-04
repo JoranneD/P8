b5032270356f834d3ed480e8739f4db1932f608e
Joranne <dolcie.joranne@gmail.com>
2024-05-05 01:40:22 +0200

    ADD// ajout de values.js + css complet de la page about + modif de banner.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Accomodation from './pages/Accomodation/Accomodation';
-//import Card from '../components/Main/Card/Card';
-// import About from './pages/About/About';
+import About from './pages/About/About';
 import NoPage from './pages/NoPage/NoPage';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Home/>} />
-                <Route path="/accomodation/:id" element={<Accomodation/>} />
-                {/* <Route path="/about" element={<About/>} /> */}
-                <Route path="*" element={<NoPage/>} />
+                <Route index element={<Home/>}/>
+                <Route path="/accomodation/:id" element={<Accomodation/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="*" element={<NoPage/>}/>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/components/Main/Values/Values.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Collapse from '../Card/Collapse/Collapse';
+import '../../Main/Values/_values.scss';
+
+function Values() {
+    const text= "lorem ipsum";
+
+    return (
+        <div>
+            <section className="values">
+                <Collapse title="Fiabilité">
+                    <p>{text}</p>
+                </Collapse>
+                <Collapse title="Respect">
+                    <p>{text}</p>
+                </Collapse>
+                <Collapse title="Service">
+                    <p>{text}</p>
+                </Collapse>
+                <Collapse title="Sécurité">
+                    <p>{text}</p>
+                </Collapse>
+            </section>
+        </div>
+    );
+}
+
+export default Values;
+
+//-----------------------
+// import React from 'react';
+// import Collapse from '../Card/Collapse/Collapse';
+// import '../../Main/Values/_values.scss';
+
+// function Values() {
+//     const text="lorem ipsum";
+
+//     return (
+//         <section className="values">
+//             <Collapse title="Fiabilité">
+//                 <p>{text}</p>
+//             </Collapse>
+//             <Collapse title="Respect">
+//                 <p>{text}</p>
+//             </Collapse>
+//             <Collapse title="Service">
+//                 <p>{text}</p>
+//             </Collapse>
+//             <Collapse title="Sécurité">
+//                 <p>{text}</p>
+//             </Collapse>
+//         </section>
+//     );
+// }
+
+// export default Values;
